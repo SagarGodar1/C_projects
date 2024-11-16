1002a1c1237ce307117becf7c2267206c9204975
Sarala Neupane <saralaneupane@SaralanoMacBook-Air.local>
2024-11-16 16:41:30 +0545

    first comment

---
+++ AreaOfSquare.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Area of Square
+int main() {
+    int side ;
+    printf("Enter the side value : ");
+    scanf("%d", &side);
+    int AreaOfSquare = side * side;
+    printf("The area of square is : %d \n", AreaOfSquare);
+    return 0;
+
+}
---
+++ SumOfTwoNumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Sum of two numbers
+int main() {
+    int a , b;
+    printf("Enter first number = ");
+    scanf("%d", &a);
+    printf("Enter second number = ");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("The sum is : %d \n", sum);
+    return 0;
+
+}
