8f3b17f0fbd0dfc33be1c1c4f7d860c6d3b0f215
SagarGodar1 <sagar.godar20701.sg@gmail.com>
2024-11-16 17:48:14 +0545

    area of circle is added

---
+++ AreaOfCircle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Area of Square
+int main() {
+   float radius;
+   printf("Enter the radius : ");
+   scanf("%f", &radius);
+   printf("The area of circle is : %f \n", 3.14 * radius * radius);
+    return 0;
+
+}
